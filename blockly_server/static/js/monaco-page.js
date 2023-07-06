fdb1a70cb193edd04edeeb0729fad83192fc46ee
Dimitris Charitos <it21395@hua.gr>
2023-07-06 20:27:01 +0300

    fix front-end responsiveness, clean code

--- blockly_server/static/js/monaco-page.js
+++ blockly_server/static/js/monaco-page.js
@@ -1,70 +1,99 @@
 var editor;
-			
+
 require.config({ paths: { vs: '../static/js/monaco-editor/min/vs' } });
 
 require(['vs/editor/editor.main'], function () {
-    editor = monaco.editor.create(document.getElementById('container'), {
-        value: ['#The robot object is "robot"', '#Example of usage: ', 'robot.move_forward_default()'].join('\n'),
-        language: 'python',
-        theme: 'vs-dark',
-        fontSize: '18px'
-    });
+  editor = monaco.editor.create(document.getElementById('container'), {
+    value: ['#The robot object is "robot"', '#Example of usage: ', 'robot.move_forward_default()'].join('\n'),
+    language: 'python',
+    theme: 'vs-dark',
+    fontSize: '18px'
+  });
 });
 
 function getCode() {
-    var code = editor.getValue();
-    var terminal = document.getElementById('terminal_scrollable-content');
+  var code = editor.getValue();
+  var terminal = document.getElementById('terminal_scrollable-content');
 
-    var codeLines = code.split('\n');  // Split the code string into an array of lines
-    for (var i = 0; i < codeLines.length; i++) {
-        // Print each line separately
-        terminal.innerHTML += '<p>' + codeLines[i] + '</p>';
-    }
-    return code;
+  var codeLines = code.split('\n');  // Split the code string into an array of lines
+  for (var i = 0; i < codeLines.length; i++) {
+    // Print each line separately
+    terminal.innerHTML += '<p>' + codeLines[i] + '</p>';
+  }
+  return code;
 }
 
 //send the code from thw workspace to be run in the robot 
 async function runCode(id) {
-    let monaco_code = editor.getValue();
-    
-    
-    if (monaco_code == "") {
-      console.log("no code to run");
-  
-      //show modal
-      showModalError("No code detected!")
-      return;
-    }
+  let monaco_code = editor.getValue();
 
-    alert(monaco_code);
-  
-    const result = await sendMonacoCode(id, monaco_code)
-    const status = result.status
-    if (status == 'started') {
-      showModalSuccess("The program running successfully!");
-    }
-    // let xmlDom = Blockly.Xml.workspaceToDom(Blockly.mainWorkspace);
-    // let xmlText = Blockly.Xml.domToPrettyText(xmlDom);
-  
-    // const result_save = await saveXml(id, xmlText);
-  
-    // if (result_save.status == 200) {
-    //   console.log("The program running successfully!");
-    // }
-  }
-  
-  //stop the code that was being exeuted in the robot 
-  function stop_script() {
-    stopScript();
+
+  if (monaco_code == "") {
+    console.log("no code to run");
+
+    //show modal
+    showModalError("No code detected!")
+    return;
   }
 
-  async function save_xml(id) {
-    let xmlText = getCode();
-    const result = await saveXml(id, xmlText)
-    const status = result.status
-    if (status == '200') {
-      showModalSuccess("Project Saved!");
+  alert(monaco_code);
+
+  const result = await sendMonacoCode(id, monaco_code)
+  const status = result.status
+  if (status == 'started') {
+    showModalSuccess("The program running successfully!");
+  }
+  // let xmlDom = Blockly.Xml.workspaceToDom(Blockly.mainWorkspace);
+  // let xmlText = Blockly.Xml.domToPrettyText(xmlDom);
+
+  // const result_save = await saveXml(id, xmlText);
+
+  // if (result_save.status == 200) {
+  //   console.log("The program running successfully!");
+  // }
+}
+
+//stop the code that was being exeuted in the robot 
+function stop_script() {
+  stopScript();
+}
+
+async function save_xml(id) {
+  let xmlText = getCode();
+  const result = await saveXml(id, xmlText)
+  const status = result.status
+  if (status == '200') {
+    showModalSuccess("Project Saved!");
+  } else {
+    showModalError("Error on saving!")
+  }
+}
+
+
+// Open accordion tabs
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
     } else {
-      showModalError("Error on saving!")
+      panel.style.maxHeight = panel.scrollHeight + "px";
     }
-  }
+  });
+}
+
+// Copy function
+function copyFunction(functionName) {
+  const textarea = document.createElement('textarea');
+  textarea.value = functionName;
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand('copy');
+  document.body.removeChild(textarea);
+
+  alert('Copied: ' + functionName);
+}
