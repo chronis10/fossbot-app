a6f1469400606ae84b6418911f456de1a79b0953
Dimitris Charitos <it21395@hua.gr>
2023-07-07 11:23:03 +0300

    load saved project from db for monaco editor

--- blockly_server/static/js/monaco-page.js
+++ blockly_server/static/js/monaco-page.js
@@ -23,6 +23,32 @@ function getCode() {
   return code;
 }
 
+function loadXml(code) {
+  editor.setValue(code);
+}
+
+async function loadProject() {
+  const url_str = window.location.href;
+  console.log(url_str)
+
+  var url = new URL(url_str);
+  var id = url.searchParams.get("id");
+  console.log("project is id", id);
+
+  if (id) {
+    //only if the project is saved, it will have an id , so we can retrieve the xml code 
+    //get project code from BE based on id 
+    const result = await sendXml(id);
+    console.log("result", result)
+    if (result.status == '200')
+      loadXml(result.data);
+    else {
+      console.log('Error when getting project\n', err);
+      showModalError("Error on code loading!");
+    }
+  }
+}
+
 //send the code from thw workspace to be run in the robot 
 async function runCode(id) {
   let monaco_code = editor.getValue();
